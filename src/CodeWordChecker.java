4f9a8ab014016e371d8e824241e75ac27252f8dc
BT_1N3_32@BKTECH.LOCAL <BT_1N3_32@BKTECH.LOCAL>
2018-11-19 14:50:07 -0500

    Initial commit

---
+++ src/CodeWordChecker.java
@@ -0,0 +1,13 @@
+public class CodeWordChecker implements StringChecker
+{
+  private String nope;
+  private int start;
+  private int end;
+  public CodeWordChecker
+  this.nope = nope;
+
+    @Override
+    public boolean isValid(String str) {
+        return false;
+    }
+}
