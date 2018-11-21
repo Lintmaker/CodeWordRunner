6e60d7c6fdf18dbd4f60b800e0d2396af744a11a
Lin Yao Pan <lpan7932@bths.edu>
2018-11-20 21:55:53 -0500

    isValid has issues

--- src/CodeWordChecker.java
+++ src/CodeWordChecker.java
@@ -1,13 +1,30 @@
-public class CodeWordChecker implements StringChecker
-{
+public class CodeWordChecker implements StringChecker {
   private String nope;
   private int start;
   private int end;
-  public CodeWordChecker
-  this.nope = nope;
+
+  public CodeWordChecker(int minLen, int maxLen, String symbol)
+  {
+    this.nope = symbol;
+    this.start = minLen;
+    this.end = maxLen;
+  }
+  public CodeWordChecker(String symbol)
+  {
+    this.nope = symbol;
+  }
 
     @Override
-    public boolean isValid(String str) {
-        return false;
+    public boolean isValid(String str)
+    {
+      if (str.indexOf(this.nope) == -1 && str.length() >= this.start && str.length() <= this.end)
+      {
+        return true;
+      }
+      else if (str.indexOf(this.nope) == -1 && str.length() <= 20 && str.length() >= 6)
+      {
+        return true;
+      }
+      return false;
     }
 }
--- src/CodeWordRunner.java
+++ src/CodeWordRunner.java
@@ -2,8 +2,8 @@ public class CodeWordRunner
 {
     public static void main ( String[] args)
     {
-        StringChecker sc1 = new CodeWordChecker();
-        StringChecker sc2 = new CodeWordChecker();
+        StringChecker sc1 = new CodeWordChecker(5,6,"$");
+        StringChecker sc2 = new CodeWordChecker("pass");
         System.out.println(sc1.isValid("happy"));
         System.out.println(sc1.isValid("happy$"));
         System.out.println(sc1.isValid("Code"));
